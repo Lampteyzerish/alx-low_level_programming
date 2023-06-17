39ffa231af8875ea0cab224e8f5ce4d875fce967
Zerish Lamptey <lampteyzerish4@gmail.com>
2023-06-16 20:00:32 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main () {
+	srand(time(NULL));
+	int n = rand () % RAND_MAX -RAND_MAX/2
+		std::cout << "is positive" << std::endl;
+} else if (n ==0) {std::count << "is zero" << std ::endl;
+} else {
+	std::cout << "is zero" << std::endl;
+}
+return 0;
+}
