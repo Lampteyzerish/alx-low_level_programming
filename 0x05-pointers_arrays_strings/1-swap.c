97da06f30fef92d2db9767f817db9b6296b88320
Zerish Lamptey <lampteyzerish4@gmail.com>
2023-06-26 16:48:00 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *swap_int - take in two variable integers and swaps them author Gagaboss
+ *@a:swaps and stores address of b
+ *@b:swapa and stores address of a
+ *Return:0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int chapter;
+
+	change = *b;
+	*b = *a;
+	*a = change;
+}
