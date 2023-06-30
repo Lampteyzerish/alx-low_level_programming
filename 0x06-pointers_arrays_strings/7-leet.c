18cfb7c6dbb0df1a29b1986d46f0b04e27d70ac4
Zerish Lamptey <lampteyzerish4@gmail.com>
2023-06-30 00:42:55 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,37 +1,36 @@
 #include "main.h"
-
 /**
-* leet - function that encodes a string into 1337.
-* letters a and A are replsced by 4.
-* letters e and E are replacesd by 3.
-* letters o and O are replaced by 0.
-* letters t and T are replaced by 7.
-* letters l and L are replaced by 1.
-* @s: pointer to string.
-*
-* Return: pointer to s.
-*/
-char *leet(char *s).
+ *leet - encodes a string into 1337.
+ *letters a and A are replaced by 4.
+ *Letters e and E are replaced by 3
+ *Letters o and O are replaced by 0
+ *Letters t and T are replaced by 7
+ *Letters l and L are replaced by 1
+ *@s: pointer to string.
+ *
+ *Return: pointer to s.
+ */
+char *leet(char *s)
 {
-int string_length. leetcount;
-char leetletters[] = "aAeEoOtTlL";
-char leetNums[] = "4433007711";
-/*
-* scan through string */
-string_length = 0;
-while (s[string_length] != '\0')
-/* check whether leetletter is found */
-{
-lettCount = 0;
-while (leeCount < 10)
-{
-if (leetLetters[leetCount] == s[string_length])
-{
-s[string_length] = leetNum[leeCount];
-}
-leetCount++;
-}
-string_length++;
-}
-return (s);
+	int stringCount, leetCount;
+	char leetLetters[] = "aAeEoOtTlL";
+	char leetNums[] = "4433007711";
+
+/*  scan through string */
+	stringCount = 0;
+	while (s[stringCount] != '\0')
+/* check whether leetLetter is found */
+	{
+		leetCount = 0;
+		while (leetCount < 10)
+		{
+			if (leetLetters[leetCount] == s[stringCount])
+			{
+				s[stringCount] = leetNums[leetCount];
+			}
+			leetCount++;
+		}
+		stringCount++;
+	}
+	return (s);
 }
