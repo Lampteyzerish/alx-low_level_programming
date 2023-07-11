77e8a6b511d6ce1bbaf127d4aeb56ebb8a793fc7
Zerish Lamptey <lampteyzerish4@gmail.com>
2023-07-10 21:05:11 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,4 @@
+- Prototype: int **alloc_grid(int width, int height);
+- Each element of the grid should be initialized to 0
+- The function should return NULL on failure
+- If width or height is 0 or negative, return NULL
