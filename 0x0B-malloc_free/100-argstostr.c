ffbcb677a17b6882a1fbb3175d35988a258387f9
Zerish Lamptey <lampteyzerish4@gmail.com>
2023-07-10 21:12:48 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,4 @@
+- Prototype: char *argstostr(int ac, char **av);
+- Returns NULL if ac == 0 or av == NULL
+- Returns a pointer to a new string, or NULL if it fails
+- Each argument should be followed by a \n in the new string 
